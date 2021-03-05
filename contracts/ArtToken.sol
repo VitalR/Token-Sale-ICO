6d0ae6286adea7fa45202244730262d5c63a88d1
VitalR <vitalii.rodikov@gmail.com>
2021-03-05 20:27:00 +0200

    Adds Transfer Tokens func

--- contracts/ArtToken.sol
+++ contracts/ArtToken.sol
@@ -1,15 +1,36 @@
 pragma solidity ^0.5.1;
 
 contract ArtToken {
-    // total token supply
+    // Name of the Token
+    string public name = "Art Token";
+    // Symbol of the Token
+    string public symbol = "ART";
+    string public standart = "Art Token v1.0";
+    // Total token supply
     uint256 public totalSupply;
+    
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
 
     // Contructor
-    constructor() public {   
-        totalSupply = 1000000;
-
-    }
     // Set the total number of tokens
-
+    constructor(uint256 _initialSupply) public {   
+        // allocat the initial supply
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
     // Read the total number of tokens
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Not enough funds.");
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const ArtToken = artifacts.require("./ArtToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ArtToken);
+  deployer.deploy(ArtToken, 1000000);
 };
