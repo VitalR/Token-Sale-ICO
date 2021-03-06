25cddf4579bc59beae464ba120029a0f6c98c1f3
VitalR <vitalii.rodikov@gmail.com>
2021-03-06 14:34:19 +0200

    Adds Delegated Transfer func

--- contracts/ArtToken.sol
+++ contracts/ArtToken.sol
@@ -10,26 +10,52 @@ contract ArtToken {
     uint256 public totalSupply;
     
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     // Contructor
     // Set the total number of tokens
+    // Read the total number of tokens
     constructor(uint256 _initialSupply) public {   
         // allocat the initial supply
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-    // Read the total number of tokens
 
     // Transfer
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value, "Not enough funds.");
-        
+
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    // approve
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transfer from
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[_from] >= _value, "Not enough funds.");
+        require(allowance[_from][msg.sender] >= _value, "Cannot transfer value larger than approved amount.");
+        // Change balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;    
+        // Update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
