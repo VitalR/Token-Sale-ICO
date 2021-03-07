79d46ac0e070b5715fcd5f5f4ef879098c2f62b3
VitalR <vitalii.rodikov@gmail.com>
2021-03-07 18:09:49 +0200

    Buy Token func has been finished

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 const ArtToken = artifacts.require("./ArtToken.sol")
-const ArtTokenSale = artifacts.require("./ArtTokenSale.sol")
+const TokenSale = artifacts.require("./TokenSale.sol")
 
 module.exports = function(deployer) {
   deployer.deploy(ArtToken, 1000000).then(function() {
     // Token price is 0.001 Ether
     let tokenPrice = 1000000000000000;
-    return deployer.deploy(ArtTokenSale, ArtToken.address, tokenPrice)
+    return deployer.deploy(TokenSale, ArtToken.address, tokenPrice)
   })
 }
